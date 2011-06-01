7c2e849b2eea3bd4e4079bef551f6d62fb64da7f
hamhei <hamhei.horiuchi@gmail.com>
2011-06-01 15:01:50 +0900

    add: get_tweet & tweet function

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,19 +1,22 @@
+# -*- encoding: UTF-8 -*-
+
 require 'rubygems'
 require 'time'
 gem 'twitter4r'
 require 'twitter'
 class DashboardController < ApplicationController
+  @@css_members = "@dai___chi @monja415 @hamhei "
+  @@juiz_url = "http://hamhei.com/juiz"
 
   def overview
     get_tweet
-    @requests = Request.find(:all, :order => 'id DESC')
+    @requests = Request.find(:all, :order => 'tweet_datetime DESC')
   end
 
 
   def noblesse_oblige
     client = client_init
-    css_members = "@monja415 @dai___chi @hamhei "
-    tweet = "@" + params["request_user"] + " "  + params["tweet"] + css_members
+    tweet = "@" + params["request_user"] + " "  + params["tweet"] + @@css_members
     client.status(:post, tweet)
     req = Request.find(params["request_id"])
     req.tweet_url = tweet
@@ -24,28 +27,33 @@ class DashboardController < ApplicationController
   private
   def get_tweet
     client = client_init
-    tweets = client.timeline_for(:replies).reverse
+    tweets = client.timeline_for(:replies)
     p tweets
-    db_tweet = Request.last
-    recent_tweet_id = 0
+
+    db_tweet = Request.find(:all, :order => 'tweet_datetime DESC').first
+    recent_tweet_head = 0 # tweets.length
     unless db_tweet.nil?
       tweets.each_with_index do |t, i|
-        if t.id_str.to_i == db_tweet.tweet_id
-          recent_tweet_id = i + 1
+        if t.id_str == db_tweet.tweet_id
+	      recent_tweet_head = i
           break
         end
       end
     end
-    if recent_tweet_id <= tweets.length
-      tweets[recent_tweet_id, tweets.length].each do |t|
-        Request.create(
-          :tweet             => t.text,
-          :tweet_id          => t.id_str.to_i,
-          :profile_image_url => t.user.profile_image_url,
-          :noblesse          => t.user.screen_name,
-          :tweet_datetime    => t.created_at
-        )
+
+	p recent_tweet_head
+    if recent_tweet_head > 0
+      tweets[0, recent_tweet_head].each do |t|
+        req = Request.create(
+                :tweet             => t.text,
+                :tweet_id          => t.id_str,
+                :profile_image_url => t.user.profile_image_url,
+                :noblesse          => t.user.screen_name,
+                :tweet_datetime    => t.created_at
+              )
       end
+      notify_str = @@css_members + "新しい依頼が届いてます。Noblesse Oblege. 今後もあなた樣方が仲睦まじい救世主であらんことを切に願います. " + @@juiz_url
+      client.status(:post, notify_str)
     end
    # redirect_to juiz_overview_path
   end
